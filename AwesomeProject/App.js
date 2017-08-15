24ca631a923eaf2f559e8b291e318c6c1e826e6b
Felicia Garcia <feliciagarcia@Felicias-MacBook-Pro.local>
2017-08-15 14:35:34 -0400

    added styles

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -23,5 +23,15 @@ export default class FlatListBasics extends Component {
     );
   }
 }
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+})
 AppRegistry.registerComponent('AwesomeProject', () => FlatListBasics);
