655f1825af660f4162c51aa0ab7d8a4dce879579
Felicia Garcia <feliciagarcia@Felicias-MacBook-Pro.local>
2017-08-15 14:34:59 -0400

    added flatlistbasics class

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,5 +1,27 @@
 import React, { Component } from 'react';
 import { AppRegistry, FlatList, StyleSheet, Text, View } from 'react-native';
 
+export default class FlatListBasics extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+      <FlatList
+        data={[
+          {key: 'Devin'},
+          {key: 'Jackson'},
+          {key: 'James'},
+          {key: 'Joel'},
+          {key: 'John'},
+          {key: 'Jillian'},
+          {key: 'Jimmy'},
+          {key: 'Julie'},
+
+        ]}
+        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+        />
+      </View>
+    );
+  }
+}
 
 AppRegistry.registerComponent('AwesomeProject', () => FlatListBasics);
